a46e47ec6a92528fdebae54d8fba6040273584f6
C0d3-5t3w <stewy1994@gmail.com>
2025-04-05 15:02:33 -0700

    Update  Changes to be committed: 	modified:   index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ header("timezone: PMT");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ziggy - Cat Extraordinaire</title>
+    <title>Ziggy</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap">
     <link rel="stylesheet" href="assets/css/main.css">
     <script src="assets/js/main.js" defer></script>
