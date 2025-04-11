6e72431d051ec6da93f5119631b7abcc04adbfa5
C0d3-5t3w <stewy1994@gmail.com>
2025-04-11 10:51:03 -0700

    Update  Changes to be committed: 	modified:   .github/workflows/static.yml 	new file:   404.php

---
+++ 404.php
@@ -0,0 +1,20 @@
+<?php include 'pkg/pages/template-head.php'; ?>
+
+<div class="container page-content">
+    <div class="section">
+        <div class="container">
+            <h1 class="section-title">404 - Page Not Found</h1>
+            
+            <div class="card">
+                <div class="card-content" style="text-align: center;">
+                    <p>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
+                    <img src="pkg/assets/images/ziggy-sad.png" alt="Sad Ziggy" style="max-width: 200px; margin: 30px auto;">
+                    <p>Let's get you back to a working page!</p>
+                    <a href="/" class="btn">Go to Homepage</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include 'pkg/pages/template-footer.php'; ?>
